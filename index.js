d041bdffd819dd4a7e9884649e316b9fac9f780d
Josh Lillywhite <JoshDLWhite@gmail.com>
2026-09-02 09:23:50 -0700

    js addition - created dynamic function to fetch and display each users information on their own user card. having issues with getting the website transition to work due to Uncaught SyntaxError: Failed to set the 'href' property on'Location': 'http://127.0.0.1:5500}user.html' is not a valid URL. on line 15

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-APIs:
-
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
+
+async function main () {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users")
+  const usersData = await users.json();
+  userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");//.join sets the innerHTML
+} 
+
+  main();
+
+function showUserPosts(id) {    //This chunk of code is not functioning like the video. There is an error having to do with the href not being seen.
+  localStorage.setItem("id", id) //items in the perenthasies are called keys. first key is whatever I want, second key is the designated variable. "id" in this case. 
+  window.location.href = `${window.location.origin}}/user.html`
+}
+
+  function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.id})">
+    <div class="user-card__container">
+      <h3>${user.name}</h4>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+        ${user.website}
+        </a></p>
+      </div>
+    </div>`;
+  }
--- user.js
+++ user.js
@@ -1,1 +1,11 @@
-// user.js code goes in here
+// console.log(localStorage.getItem("id"));   removed
+
+async function main() {
+  const id = localStorage.getitem("id")
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+  const postsData = await posts.json();
+
+  console.log(postsData)
+}
+
+main();
